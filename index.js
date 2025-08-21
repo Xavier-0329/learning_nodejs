9b66734daa2365a77567a795fe4f4cc993be6374
Xavier.L <xavierlee0329@gmail.com>
2025-08-21 13:53:34 +0800

    update frontend

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
 
-const cors = require("cors");
 app.use(cors());
 
+app.use(express.static('dist'))
 
 let notes = [
   {
